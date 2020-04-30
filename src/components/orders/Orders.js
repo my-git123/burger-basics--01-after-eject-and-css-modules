b31ee906ee62506e75c9be0efcbdbac176de9e9e
Jaya Sharma <jaya.sharma2k7@gmail.com>
2020-04-30 14:31:38 -0700

    fetch orders from server

--- src/components/orders/Orders.js
+++ src/components/orders/Orders.js
@@ -1,47 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Order from '../order/Order';
-import axios from '../../axios-orders';
+import { fetchOrders} from '../../actions/orderAction';
+import Spinner from '../ui/spinner/Spinner';
+//import axios from '../../axios-orders';
 
-const Orders = props => {
- const [orderState, setOrderState] = useState({
-     orders:[],
-     loading: true
- });
+const Orders = ({orders,loading,fetchOrders}) => {
+//  const [orderState, setOrderState] = useState({
+//      orders:[],
+//      loading: true
+//  });
  //const [loading,setLoading] = useState(ftrue);
-const {orders, loading} = orderState;
 
-    useEffect(() => {
-        const fetchData = async () => { 
-            try {
-                const res = await axios.get('/orders.json');
-                const fetchedOrders = [];
-                for ( let key in res.data) {
-                    //fetchedOrders.push(res.data[key]);
-                    fetchedOrders.push({...res.data[key], id:key});
-                }
-                 setOrderState({loading:false,orders: fetchedOrders});
-                 console.log(fetchedOrders);
-            } catch (err) {
-                 setOrderState({loading:false});
-            }
-                }
-        fetchData();
-    },[]);
-    
+useEffect(() => {
+    fetchOrders();
+},[]);
 
-    return (
-        <div >
-          {orders.map(order => (<Order key = {order.id}
+    // useEffect(() => {
+    //     const fetchData = async () => { 
+    //         try {
+    //             const res = await axios.get('/orders.json');
+    //             const fetchedOrders = [];
+    //             for ( let key in res.data) {
+    //                 //fetchedOrders.push(res.data[key]);
+    //                 fetchedOrders.push({...res.data[key], id:key});
+    //             }
+    //              setOrderState({loading:false,orders: fetchedOrders});
+    //              console.log(fetchedOrders);
+    //         } catch (err) {
+    //              setOrderState({loading:false});
+    //         }
+    //             }
+    //     fetchData();
+    // },[]);
+    let theOrders = <Spinner />;
+    if(!loading) {
+        theOrders = orders.map(order => (<Order key = {order.id}
                                        ingredients = {order.ingredients}
-                                       price = {order.price}/>))}
-            
-        </div>
-    )
+                                       price = {order.price}/>))
+                 }
+        
+    return (
+        <div>{theOrders}</div>
+    );
 }
 
 Orders.propTypes = {
-
+  fetchOrders:PropTypes.func.isRequired,
+  orders: PropTypes.object.isRequired,
+  loading:PropTypes.bool.isRequired
 }
 
-export default Orders
+const mapStateToProps = state =>({
+    orders: state.orderReducer.orders,
+    loading: state.orderReducer.loading
+});
+
+export default connect(mapStateToProps,{fetchOrders})(Orders);
